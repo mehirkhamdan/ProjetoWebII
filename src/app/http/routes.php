a14bb5b5ba14c7f14f2a1b7e208706605e68dd03
superior <superior@LAB2-PC15>
2019-10-11 19:54:18 -0300

    criar banco dados

--- src/app/http/routes.php
+++ src/app/http/routes.php
@@ -1,1 +1,9 @@
-<?php
+<?php 
+
+//add route
+
+$app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
+    $name = $args['name'];
+    $response->getBody()->write("Hello, $name");
+    return $response;
+});
---
+++ src/config/database.php
@@ -0,0 +1,9 @@
+define('path', $path = array($_SERVER['DOCUMENT_ROOT'] . "\..\app\model\\entity"));
+define('dbParams',
+    array(
+        'driver'   => 'pdo_mysql',
+        'user'     => 'root',
+        'password' => '',
+        'dbname'   => 'projeto_ooii',
+    )
+);
